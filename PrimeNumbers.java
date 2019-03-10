170d546db2e24daaf84f641016c2b5259e8c547e
TinaBstr <48390234+TinaBstr@users.noreply.github.com>
2019-03-10 19:13:11 +0300

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+  class PrimeNumbers {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int top = scanner.nextInt();
+    for (int i=2;i<top;i++){
+        if(checkSimple(i))
+            System.out.println(i);
+    }
+  }
+
+  public static boolean checkSimple(int i){
+    if (i<=1)
+        return false;
+    else if (i <=3)
+        return true;
+    else if (i%2==0 || i %3 ==0)
+        return false;
+    int n = 5;
+    while (n*n <=i){
+        if (i % n ==0 || i % (n+2) == 0)
+            return false;
+        n=n+6;
+    }
+    return true;
+  }
+}
